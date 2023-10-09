3d057077792f85af6b70547ed741876394774991
alexandria-lukachi-le <alexandria777le@gmail.com>
2023-10-09 16:38:00 -0500

    Start script with const and api url source

---
+++ script.js
@@ -0,0 +1,14 @@
+const pokemonList = document.querySelector("#pokemonList");
+const buttonHeader = document.querySelectorAll(".btn-header");
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+for (let i = 1; i <= 248; i++) {
+    fetch(URL + 1)
+        .then((response) => response.json())
+        .then(data => showPokemon(data))
+}
+
+function showPokemon(poke) {
+
+    let types = poke.types.map((type) => `<p class=${type.type.name}type>${type.type.name}</p>`)
+}
