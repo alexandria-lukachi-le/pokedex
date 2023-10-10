d4eac01f6a926ea1a75ac3d0b1344816da3c577b
alexandria-lukachi-le <alexandria777le@gmail.com>
2023-10-09 19:29:06 -0500

    Fix url fetch line error
    Page showing all pokemon would only show bulbasaur repeatedly. The error was a '1' where an 'i' should be.

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const buttonHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
 for (let i = 1; i <= 248; i++) {
-    fetch(URL + 1)
+    fetch(URL + i)
         .then((response) => response.json())
         .then(data => showPokemon(data))
 }
 
 function showPokemon(poke) {
 
-    let types = poke.types.map((type) => `<p class=${type.type.name}type>${type.type.name}</p>`)
+    let types = poke.types.map((type) => `<p class="${type.type.name} type">${type.type.name}</p>`);
     types = types.join('');
 
-    let pokeId = poke.id.string();
+    let pokeId = poke.id.toString();
     if (pokeId.length === 1) {
         pokeId = "00" + pokeId;
     } else if (pokeId.length === 2) {
@@ -23,26 +23,45 @@ function showPokemon(poke) {
     const div = document.createElement("div");
     div.classList.add("pokemon");
     div.innerHTML = `
-
-    <p class="pokemon-id-back">${pokeId}</p>
-    <div class="pokemon-image">
-        <img src="${poke.sprites.front_default}" alt="${poke.name}">
-    </div>
-    <div class="pokemon-info">
-        <div class="name-container">
-            <p class="pokemon-num">${pokeId}</p>
-            <h2 class="pokemon-name">${poke.name}</h2>
+        <p class="pokemon-id-back">${pokeId}</p>
+        <div class="pokemon-image">
+            <img src="${poke.sprites.front_default}" alt="${poke.name}">
         </div>
-        
-        <div class="pokemon-type">
-            ${types}
-        </div>
-
+        <div class="pokemon-info">
+            <div class="name-container">
+                <p class="pokemon-num">${pokeId}</p>
+                <h2 class="pokemon-name">${poke.name}</h2>
+            </div>
+            <div class="pokemon-type">
+               ${types}
+             </div>
         <div class="pokemon-stats">
-        <p class="stat">${poke.height}</p>
-        <p class="stat">${poke.weight}</p>
+            <p class="stat">${poke.height}</p>
+            <p class="stat">${poke.weight}</p>
         </div>
     </div>
     `;
     pokemonList.append(div);
 }
+
+buttonHeader.forEach(button => button.addEventListener("click", (event) => {
+    const buttonId = event.currentTarget.id;
+
+    pokemonList.innerHTML = "";
+
+    for (let i = 1; i <= 248; i++) {
+        fetch(URL + i)
+            .then((response) => response.json())
+            .then(data => {
+                
+                if(buttonId === "All") {
+                    showPokemon(data);
+                } else {
+                    const types = data.types.map(type => type.type.name);
+                    if (types.some(type => type.includes(buttonId))) {
+                        showPokemon(data);
+                    }
+                }
+            })
+    }
+}))
